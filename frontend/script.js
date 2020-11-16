7464238c5d74cc189e5cdefc9f36fe5a8ab7b29d
Lucas Sta Maria <lucas.stamaria@gmail.com>
2020-11-16 18:36:56 +0800

    fix: center room inputs
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -6,12 +6,10 @@ Vue.component("lobby", {
     },
     template: 
     `<div id="join-room">
-    <div>
-        <input v-model="roomCode" placeholder="Room Code">
-    </div>
-    <div>
-        <button v-on:click="joinRoom">Join Room</button>
-        <button v-on:click="createRoom">Create Room</button>
+    <input id="roomcodeinput" maxlength="6" v-model="roomCode" placeholder="Room Code">
+    <div id="roombuttons">
+        <button class="roombutton" v-on:click="joinRoom">Join Room</button>
+        <button class="roombutton" v-on:click="createRoom">Create Room</button>
     </div>
     </div>`,
     methods: {
