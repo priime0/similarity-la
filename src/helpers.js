8bcccccc2ba63120e4a3399a32cbf30665f7d369
Lucas Sta Maria <lucas.stamaria@gmail.com>
2020-11-27 14:01:44 +0800

    fix: create helper for generating shows list
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- src/helpers.js
+++ src/helpers.js
@@ -22,14 +22,30 @@ function createRoom (roomCode, admin) {
         users: [{
             name: admin,
             choices: [],
-        }]
+        }],
+        showsList: [],
     };
 
     return room;
+}
+
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function generateShowList () {
+    const MAX_SHOWS = 10;
+    const showList = require("./list");
+    for (let i = showList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i+1));
+        const x = showList[i];
+        showList[i] = showList[j];
+        showList[j] = x;
+    }
+    
+    return showList.slice(0, MAX_SHOWS);
 }
 
 module.exports = {
     generateRoomCode,
     roomExists,
     createRoom,
+    generateShowList,
 }
